db29fc5bef33c104cc82dae860b2885197ba34a2
nethmishehara <nshehara0921@gmail.com>
2025-09-23 20:18:16 +0530

    deleted theme agent

--- themeAgent.js
+++
@@ -1,5 +0,0 @@
-
-print("Feedback:", f, "-> Sentiment:", analyzer(f)[0])
-print("Feedback:", f, "-> Sentiment:", analyzer(f)[0])
-print("Feedback:", f, "-> Sentiment:", analyzer(f)[0])
-print("Feedback:", f, "-> Sentiment:", analyzer(f)[0])
